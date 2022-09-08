405e0543e981965843083aba7e1952aab36897d7
Alexander Cherepanov <super.sany96@yandex.ru>
2022-09-08 11:58:10 +0300

    стали выводиться товары,сервер и код работают

--- eshop.js
+++ eshop.js
@@ -5,6 +5,11 @@ $('document'). ready(function(){
 function loadGoods() {
     //загружаю товар на страницу
     $.getJSON('goods.json', function (data) {
-        
+        var out = '';
+        for (var key in data){
+            out+='<h3>'+data[key]['name']+'</h3>';
+            out+='<p>'+data[key]['cost']+'</p>';
+        }
+        $('#goods').html(out);
     })
 }
